0af6554c735bb55f8dda4ec1e6cb238ecf869b71
Jajav33 <to.jajav33@gmail.com>
2023-03-01 19:44:41 -0800

    only showing editor toolbar when focused for cleaner look

--- @plooma/app/src/components/NotePadComponent.vue
+++ @plooma/app/src/components/NotePadComponent.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <q-input v-model="mainStore.profiles[mainStore.currentProfile].nodes[props.nodeUID].nodeTitle" filled dense></q-input>
-    <q-editor v-model="text"></q-editor>
+    <q-editor @blur="onBlur" @focus="onFocus" v-model="text" :toolbar="shouldShowToolbar ? undefined : []"></q-editor>
   </div>
 </template>
 
 <script lang="ts">
 import {
   defineComponent,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  ref
 } from 'vue';
 import { useMainStore } from '../stores/mainStore';
 
@@ -21,13 +24,21 @@ export default defineComponent({
     nodeUID: {
       type: String,
       required: true
-    },
+    }
   },
   setup(props) {
     const mainStore = useMainStore();
     const text = mainStore.profiles[props.profileName].nodes[props.nodeUID].htmlText;
-
-    return {text, props, mainStore};
+    let shouldShowToolbar = ref(false);
+    return {
+      text, props, mainStore, shouldShowToolbar,
+      onFocus() {
+        shouldShowToolbar.value = true;
+      },
+      onBlur() {
+        shouldShowToolbar.value = false;
+      }
+    };
   },
 });
 </script>
