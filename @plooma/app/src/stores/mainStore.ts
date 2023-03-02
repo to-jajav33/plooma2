e8cd70e468cbe913bf6196c49d527467068931d3
Jajav33 <to.jajav33@gmail.com>
2023-03-01 20:17:50 -0800

    adding heroes journey template

--- @plooma/app/src/stores/mainStore.ts
+++ @plooma/app/src/stores/mainStore.ts
@@ -32,19 +32,40 @@ export const useMainStore = defineStore('MainStore', {
             timeline: reactive([])
           });
       }
-      const newNodeUID = this.createNewNode({profileName: this.currentProfile});
-      this.addTimelineNodeAt(newNodeUID, 0);
+
+      // heroes journey template
+      // https://www.movieoutline.com/articles/the-hero-journey-mythic-structure-of-joseph-campbell-monomyth.html
+      const templateArr = [
+        {title: 'Ordinary World'},
+        {title: 'Call To Adventure'},
+        {title: 'Refusal'},
+        {title: 'Meeting with the Mentor'},
+        {title: 'Crossing the Threshold and accepts mission'},
+        {title: 'Challenges/Obstacles'},
+        {title: 'Learn what skills are needed the Greatest Challenge...Is the hero ready for the biggest challenge yet?'},
+        {title: 'Challenge to aquire a skill(s)... Mini Boss Battle'},
+        {title: 'Reward.. (hero acuqies skill(s) and transforms to a new state)'},
+        {title: 'Road Back... is/are the Reward/skill(s) going to help solve the conflict?'},
+        {title: 'Climax... Final boss battle'},
+        {title: 'Return to Ordinary World a changed person. Receives final reward/lesson/skill/gift/etc'},
+      ];
+
+      for (const i in templateArr) {
+        const templateInfo = templateArr[i];
+        const newNodeUID = this.createNewNode({profileName: this.currentProfile, nodeTitle: templateInfo.title});
+        this.addTimelineNodeAt(newNodeUID, Number(i));
+      }
     },
     addTimelineNodeAt(nodeUID: string, indx: number) {
       this.profiles[this.currentProfile].timeline.splice(indx, 0, nodeUID);
     },
-    createNewNode(params: {profileName: string}) {
-      const {profileName} = params;
+    createNewNode(params: {profileName: string, nodeTitle?: string}) {
+      const {profileName, nodeTitle} = params;
       const nodeUID = uid();
       const newNode = {
         nodeUID,
         htmlText: '',
-        nodeTitle: 'NoTitle',
+        nodeTitle: nodeTitle || 'Node Title',
       };
       this.profiles[profileName].nodes[nodeUID] = newNode;
 
